09f4e72e65c8c3397c6eedcdb16f5352894c7447
yusufbagha <yusufbagha64@gmail.com>
2020-12-11 16:41:05 -0500

    Updates

---
+++ precache-manifest.80e182c05136bcf85e8e353519aa8a60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25de0deefb36958691f357b81ac3e9a9",
+    "url": "/yusufbagha.com/index.html"
+  },
+  {
+    "revision": "e1a0974278a199543d29",
+    "url": "/yusufbagha.com/static/css/main.7befdc6c.chunk.css"
+  },
+  {
+    "revision": "a7977628df4fc64f3969",
+    "url": "/yusufbagha.com/static/js/2.78885ac2.chunk.js"
+  },
+  {
+    "revision": "b8e0eba7740c33182cb0373640456b8c",
+    "url": "/yusufbagha.com/static/js/2.78885ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a0974278a199543d29",
+    "url": "/yusufbagha.com/static/js/main.94afe50d.chunk.js"
+  },
+  {
+    "revision": "d4abbb8b06085e8e345b",
+    "url": "/yusufbagha.com/static/js/runtime-main.3485d79b.js"
+  },
+  {
+    "revision": "1038881e949623127b3ecfc613dce63c",
+    "url": "/yusufbagha.com/static/media/YusufBagha.Resume.1038881e.pdf"
+  },
+  {
+    "revision": "1eecefea2fbffdc464f5478d52b9d8e4",
+    "url": "/yusufbagha.com/static/media/yusuf.1eecefea.png"
+  }
+]);

bad154bc14d6c5af833692d579a643f7f56f0fc9
yusufbagha <yusufbagha64@gmail.com>
2020-12-27 01:54:34 -0500

    Updates

---
+++ precache-manifest.2fce6bf5cc39c9c02a5a5947d0013679.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5af841d0e49b0893b6879a8a7971905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c03855223e357d5c4d3",
+    "url": "/static/css/main.073b89ea.chunk.css"
+  },
+  {
+    "revision": "1cde604da5dd0d096608",
+    "url": "/static/js/2.01437d70.chunk.js"
+  },
+  {
+    "revision": "cae1b573efd37b7d89c6086b35f3367f",
+    "url": "/static/js/2.01437d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c03855223e357d5c4d3",
+    "url": "/static/js/main.bbedd264.chunk.js"
+  },
+  {
+    "revision": "fcfe827e8c153e3bcda5",
+    "url": "/static/js/runtime-main.5ad136f7.js"
+  },
+  {
+    "revision": "1038881e949623127b3ecfc613dce63c",
+    "url": "/static/media/YusufBagha.Resume.1038881e.pdf"
+  },
+  {
+    "revision": "1eecefea2fbffdc464f5478d52b9d8e4",
+    "url": "/static/media/yusuf.1eecefea.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yusufbagha.com/precache-manifest.6329cd1785849137c2215a04d870b5f3.js"
+  "/precache-manifest.2fce6bf5cc39c9c02a5a5947d0013679.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yusufbagha.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

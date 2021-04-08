c8bcd5d5b5098de39da7ddcea422c6efcc845c55
yusufbagha <yusufbagha64@gmail.com>
2021-04-07 20:26:51 -0400

    updates to Resume

--- src/static/data.js
+++ src/static/data.js
@@ -7,7 +7,7 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 import { Link } from 'react-router-dom'
-import Resume from './YusufBagha.Resume.2021.pdf'
+import Resume from './Yusuf.Bagha.Resume.2021.pdf'
 import { faLinkedinIn, faGithub, } from '@fortawesome/free-brands-svg-icons'
 import { faCalendar, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
@@ -76,14 +76,14 @@ export const headerResumeButton = {
 
 export const topBanner = {
   heading: 'Actively Interviewing #OpenToWork',
-  url: 'https://www.linkedin.com/posts/yusufbagha_opentowork-covid19layoffs-softwaredeveloper-activity-6721957203298816000-Z_sx'
+  url: 'https://linkedin.com/in/yusufbagha'
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   home 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-export const homeHeadings = ['Hi, my name is Yusuf Bagha.', 'I build scalable web applications to match growing customer demand for your business.']
+export const homeHeadings = ['Hi, my name is Yusuf Bagha.', 'I build scalable web applications to match growing customer demand for your company.']
 export const homeParagraphs = ['I’m a full stack software engineer based out of New York. I have experience developing scalable, maintainable, and secure Web applications. From concept and architectural implementation, to frontend development, backend development, and devops']
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -98,10 +98,14 @@ export const workHistory = [
     startDate: 'Feb 2019',
     endDate: 'July 2020',
     descriptions: [
-      'Designed and implemented secure video upload to S3 directly from the frontend. 50K videos uploaded to date',
-      'Studied Django change logs and manually modified 10K+ lines of code to incrementally update from v1.7 to v2.1',
       'Engineered lightweight, efficient, and scalable network speed test to check if user meets minimum requirements',
-      'Increased application scalability by developing custom load testing script and identifying api inefficiencies',
+      'Studied Django change logs and manually modified 10K+ lines of code to incrementally update from v1.7 to v2.1',
+      'Accomplished GDPR compliance by making PII downloadable, and deleting user data upon account termination',
+      'Investigated production bugs reported by clients on detective duty and manually processed user gameplay data',
+      'Developed and executed migration plan to remove unnecessary data for 2 million users after testing in staging',
+      'Designed and implemented secure video upload to S3 directly from the frontend. 50K videos uploaded to date',
+      'Increased application scalability by developing custom load testing script and identifying API inefficiencies',
+      'Developed flexible and performant endpoints for data driven insight visualization using raw user trait data'
     ]
   },
   {
@@ -110,23 +114,23 @@ export const workHistory = [
     startDate: 'Aug 2018',
     endDate: 'Jan 2019',
     descriptions: [
-      'Reduced unnecessary spending by engineering a Node microservice that logs walker activity in Mongo',
-      'Recovered application source code after data loss and restored Android app to Play Store',
-      'Faced critical server failure during first week and restored services without revenue loss',
-      'Expedited dog walker hiring by developing internal automation software for human resources',
-      'Refactored application structure and implemented future bookings to increase customer spending',
+      'Reduced unnecessary spending by engineering a Node microservice that logs and reports walker activity in Mongo',
+      'Expedited dog walker hiring by developing internal automation software for human resources to match growth',
+      'Faced critical server failure during first week and manually restored services without customer or revenue loss',
+      'Refactored application structure and increased consumer spending by implementing future dog walk bookings',
+      'Recovered application source code after accidental data loss and swiftly restored Android app to Play Store'
     ]
   },
   {
     company: 'Sandbox Innovations',
     position: 'Full Stack Software Developer',
-    startDate: 'June 2018',
+    startDate: 'May 2018',
     endDate: 'Aug 2018',
     descriptions: [
-      'Engineered administrative panel with varying functionalities for teachers, principals, and district heads',
-      'Built and implemented a realtime Node chat application and into existing e-learning platform',
-      'Managed engineering team and kept product development on track with business pivots',
+      'Engineered custom administrative panel with varying functionalities for teachers and principals',
       'Developed assignment based online homework platform with automated checks and reporting',
+      'Managed engineering team and kept product development on track with business pivots',
+      'Built and integrated a real time Node chat application into existing e-learning platform'
     ]
   },
   {
@@ -135,10 +139,11 @@ export const workHistory = [
     startDate: 'Feb 2018',
     endDate: 'Feb 2018',
     descriptions: [
-      'Debugged featured Alexa skill with Amazon’s head voice engineer and deployed realtime fixes',
-      'Collaborated with senior engineer to improve performance by migrating website from WP to React',
       'Researched technical viability and developed prototypes for Alexa and Google Assistant flight skills',
+      'Collaborated with senior engineer to improve performance by migrating website from WP to React',
       'Accelerated application development cycles by switching to cross platform voice framework Jovo',
+      'Debugged featured Alexa skill with Amazon’s head voice engineer and deployed realtime fixes',
+
     ]
   },
   {
@@ -154,10 +159,10 @@ export const workHistory = [
     startDate: 'May 2016',
     endDate: 'July 2016',
     descriptions: [
-      'Engineered application architecture that anonymized server-client communication using Express',
-      'Vertically scaled Node application to handle 7000 users and debugged Mongo bottlenecks',
       'Utilized a mobile first approach to develop an app interface that automatically adapts to different devices',
       'Deployed realtime code fixes for app breaking bugs discovered by users without service interruption',
+      'Engineered application architecture that anonymized server-client communication using Express',
+      'Vertically scaled Node application to handle 7000 users and debugged Mongo bottlenecks',
     ]
   },
   {
@@ -167,8 +172,8 @@ export const workHistory = [
     endDate: 'May 2015',
     descriptions: [
       'Pushed real time updates from server to client with asynchronous JavaScript and restful design',
-      'Developed custom user interface that employs flexible layout and reactive templating',
       'Requested REST endpoints and programmed DOM manipulations based on server response',
+      'Developed custom user interface that employs flexible layout and reactive templating',
     ]
   },
   {
@@ -177,8 +182,8 @@ export const workHistory = [
     startDate: 'June 2014',
     endDate: 'July 2014',
     descriptions: [
-      'Worked with founders to design and develop a mobile friendly promotional website in WordPress',
       'Implemented custom styles on top of prefabricated theme and wrote JavaScript to create animations',
+      'Worked with founders to design and develop a mobile friendly promotional website in WordPress',
       'Modified PHP source code to add analytical functionalities and disable unwanted plugin behavior',
     ]
   },
@@ -201,6 +206,12 @@ export const aboutParagraphs = [`I'm a full stack Python &amp; JavaScript engine
 export const consultingHeadings = ['Software Engineering Consulting']
 export const consultingParagraphs = [`Bagha LLC works with clients of all shapes and sizes and across a number of industries, from finance and healthcare to the New York Mets. Whatever challenge your business faces, Bagha LLC has a solution for you. Whether you’re dealing with growing technical debt or are having difficulty scaling your product, please feel free to <a href="/contact">reach out</a> with a brief description of your current predicament and it will be my pleasure to assist you.`]
 export const clients = [
+  {
+    company: 'ShuffleThink',
+    project: '',
+    url: 'http://shufflethink.com',
+    year: '2021',
+  },
   {
     company: 'Jeffrey Modell Foundation',
     project: '',
